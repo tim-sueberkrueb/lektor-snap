2d11f6d1063be738239afd4432fee3ff62204b40
Tim Süberkrüb <dev@timsueberkrueb.io>
2017-08-24 15:46:46 +0200

    Add fake os-release file and use it Lektor uses pip for its plugin system. pip uses the /etc/os-release file to determine an user-agent. Since /etc/os-release doesn't seem to be accessible from within the snap application, we include and use a fake os-release file.

--- snap/launcher.sh
+++ snap/launcher.sh
@@ -3,4 +3,9 @@
 # Necessary to fix UnicodeDecodeError
 export LC_ALL=C.UTF-8
 
+# Necessary to make pip able
+# to read an os-release file
+# used for user-agent generation
+export UNIXCONFDIR=$SNAP/etc
+
 exec $SNAP/bin/lektor "$@"
